4915308dca401733f3aaf8a1a32dfbbc051f8ce2
Airi Taukurade <taukurade@gmail.com>
2022-06-14 04:51:31 +0500

    main

--- assets/database.py
+++ assets/database.py
@@ -5,12 +5,15 @@ from tabulate import tabulate
 from peewee import *
 os.system('color')
 
-__all__ = ["db",'Catalog','Abonements','DB']
+__all__ = ["db", 'Catalog', 'Abonements', 'DB']
 
 db = SqliteDatabase('data.db')
+
+
 class Mdl(Model):
     class Meta:
         database = db
+
 
 class Catalog(Mdl):
     ISBN = TextField()
@@ -19,10 +22,12 @@ class Catalog(Mdl):
 
 class Abonements(Mdl):
     owner = TextField()
-    
+
+
 class DB():
     def clear_screen():
         os.system('cls')
+
     def help():
         print("""
 Список доступных команд:
@@ -38,37 +43,44 @@ class DB():
     exit - выход из приложения.
     about - об авторе приложения.
         """)
+
     def get_catalog():
-        table = [['id','ISBN','Авторы','Дата публикации','Название','Текущий владелец']]
+        table = [['id', 'ISBN', 'Авторы', 'Дата публикации',
+                  'Название', 'Текущий владелец']]
         for book in Catalog.select():
             try:
-                data=requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{book.ISBN}").json()['items'][0]['volumeInfo']
+                data = requests.get(
+                    f"https://www.googleapis.com/books/v1/volumes?q=isbn:{book.ISBN}").json()['items'][0]['volumeInfo']
             except KeyError:
                 continue
-            table.append([book.id,book.ISBN,", ".join(data['authors']),data['publishedDate'],data['title'],book.Using_by])
-        print(tabulate(table,tablefmt="rst"))
-    
+            table.append([book.id, book.ISBN, ", ".join(
+                data['authors']), data['publishedDate'], data['title'], book.Using_by])
+        print(tabulate(table, tablefmt="rst"))
+
     def get_abonements():
-        table=[['Номер абонемента','Имя Фамилия владельца']]
+        table = [['Номер абонемента', 'Имя Фамилия владельца']]
         for abon in Abonements.select():
-            table.append([abon.id,abon.owner])
-        print(tabulate(table,tablefmt="rst"))
+            table.append([abon.id, abon.owner])
+        print(tabulate(table, tablefmt="rst"))
+
     def add_book():
-        ISBN=input('Введите ISBN книги>')
-        return Catalog.create(ISBN=ISBN,Using_by=0).id
+        ISBN = input('Введите ISBN книги>')
+        return Catalog.create(ISBN=ISBN, Using_by=0).id
 
     def remove_book():
-        id=int(input('Введите id книги в каталоге>'))
+        id = int(input('Введите id книги в каталоге>'))
         if Catalog.select().where(Catalog.id == id).get_or_none() is None:
             print('Книги с этим id не существует')
         Catalog.delete_by_id(int(id))
         return 0
+
     def add_abon():
-            owner=input('Введите Имя и Фамилию владельца>')
-            return Abonements.create(owner=owner).id
+        owner = input('Введите Имя и Фамилию владельца>')
+        return Abonements.create(owner=owner).id
+
     def give_book():
-        id=int(input('Введите id книги в каталоге>'))
-        ab=int(input('Введите id абонемента>'))
+        id = int(input('Введите id книги в каталоге>'))
+        ab = int(input('Введите id абонемента>'))
         if Abonements.select().where(Abonements.id == ab).get_or_none() is None:
             print('Данного абонемента не существует')
             return 1
@@ -78,16 +90,16 @@ class DB():
         elif Catalog.select().where((Catalog.id == id) & (Catalog.Using_by == ab)).get_or_none() is not None:
             print("Эта книга уже выдана на этот абонемент")
         elif Catalog.select().where((Catalog.id == id) & (Catalog.Using_by == 0)).get_or_none() is not None:
-            d=Catalog.select().where(Catalog.id == id).get()
+            d = Catalog.select().where(Catalog.id == id).get()
             d.Using_by = ab
             d.save()
             return 0
         else:
             print('Книга занята')
-        
+
     def return_book():
-        id=input('Введите id книги>')
-        ab=input('Введите id абонемента>')
+        id = input('Введите id книги>')
+        ab = input('Введите id абонемента>')
         if Catalog.select().where(Catalog.id == id).get_or_none() is None:
             print('Книги с этим id не существует')
             return 1
@@ -98,11 +110,12 @@ class DB():
             print("Эта книга не была выдана на этот абонемент")
             return 3
         else:
-            d=Catalog.select().where((Catalog.id == id) & (Catalog.Using_by == ab)).get()
+            d = Catalog.select().where((Catalog.id == id) & (Catalog.Using_by == ab)).get()
             d.Using_by = '0'
-            
+
             d.save()
             return 0
+
     def about():
         print("""
 Made with \x1b[31;1m♥\x1b[0m by Tau
--- main.py
+++ main.py
@@ -2,29 +2,29 @@
 from assets.database import *
 
 
-#Инициализация приложения
+# Инициализация приложения
 db.connect()
-db.create_tables([Catalog,Abonements])
+db.create_tables([Catalog, Abonements])
 
 
-commands={
-    'clear':DB.clear_screen,
-    'get_cat':DB.get_catalog,
-    'get_abon':DB.get_abonements,
-    'add_book':DB.add_book,
-    'rem_book':DB.remove_book,
-    'give_book':DB.give_book,
-    'ret_book':DB.return_book,
-    'exit':exit,
-    'help':DB.help,
-    'about':DB.about,
-    'add_abon':DB.add_abon,
+commands = {
+    'clear': DB.clear_screen,
+    'get_cat': DB.get_catalog,
+    'get_abon': DB.get_abonements,
+    'add_book': DB.add_book,
+    'rem_book': DB.remove_book,
+    'give_book': DB.give_book,
+    'ret_book': DB.return_book,
+    'exit': exit,
+    'help': DB.help,
+    'about': DB.about,
+    'add_abon': DB.add_abon,
 }
 
 
 def loop():
     try:
-        cmd=input('>')
+        cmd = input('>')
     except:
         exit()
     if cmd not in commands.keys():
@@ -32,4 +32,6 @@ def loop():
     else:
         commands[cmd]()
     loop()
+
+
 loop()
